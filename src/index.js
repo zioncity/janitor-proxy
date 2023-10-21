d32c76815684f4201c5fd41221b27f0e55eb9d74
dllt98 <135049225+dllt98@users.noreply.github.com>
2023-10-21 12:44:34 +0000

    add avatar proxy

--- src/index.js
+++ src/index.js
@@ -9,11 +9,30 @@ app.use(cors());
 app.get("/health", (_req, res) => res.sendStatus(200));
 
 // Define the proxy middleware
-const proxy = createProxyMiddleware("/bot-avatars", {
+const botAvatarProxy = createProxyMiddleware("/bot-avatars", {
   target: "https://pics.janitorai.com",
   changeOrigin: true,
   pathRewrite: {
-    "^/bot-avatas": "/bot-avatars",
+    "^/bot-avatars": "/bot-avatars",
+  },
+  onProxyReq(proxyReq, req, res) {
+    proxyReq.setHeader(
+      "User-Agent",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+    );
+    proxyReq.setHeader(
+      "sec-ch-ua",
+      `"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"`
+    );
+    proxyReq.setHeader("Referer", "https://www.janitorai.com");
+  },
+});
+
+const avatarProxy = createProxyMiddleware("/avatars", {
+  target: "https://pics.janitorai.com",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/avatars": "/avatars",
   },
   onProxyReq(proxyReq, req, res) {
     proxyReq.setHeader(
@@ -29,10 +48,15 @@ const proxy = createProxyMiddleware("/bot-avatars", {
 });
 
 // Use the proxy middleware
-app.use("/bot-avatars", proxy);
+app.use("/bot-avatars", botAvatarProxy);
+app.use("/avatars", avatarProxy);
 
 // Set Expires header for caching (adjust as needed)
 app.use("/bot-avatars", (req, res, next) => {
+  res.header("Cache-Control", "public, max-age=604800"); // 7 days in seconds
+  next();
+});
+app.use("/avatars", (req, res, next) => {
   res.header("Cache-Control", "public, max-age=604800"); // 7 days in seconds
   next();
 });
