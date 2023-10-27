8b41869a7fb9a70cd3c95000df815d72f48a5fab
dllt98 <135049225+dllt98@users.noreply.github.com>
2023-10-27 13:06:27 +0000

    add img

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ app.get("/proxy/:folder/:fileName", async (req, res, next) => {
   res.header("Cache-Control", "public, max-age=604800"); // 7 days in seconds
 
   const {folder, fileName} = req.params; // folder can be bot-avatars or avatars
+  console.log({folder, fileName})
+
   const imageData = await getImageBase64(folder,fileName);
 
   // Extract the file extension from the URL
@@ -27,6 +29,8 @@ app.get("/proxy/:folder/:fileName", async (req, res, next) => {
 
   // Set the response content type based on the file extension
   res.setHeader("Content-Type", contentType);
+
+  console.log('Content-Type', contentType);
   // Send the image data as the response
   res.send(Buffer.from(imageData, "base64"));
 });
