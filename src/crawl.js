15cde8f5a8f345051d5866973f231e6f9173a6a5
dllt98 <135049225+dllt98@users.noreply.github.com>
2023-10-28 05:17:12 +0000

    fix img size

--- src/crawl.js
+++ src/crawl.js
@@ -15,24 +15,42 @@ async function getChromiumPage() {
     return page;
 }
 
+function getImgType(fileName) {
+    // Extract the file extension from the URL
+    const extname = fileName.split(".").pop();
+    const contentType =
+        {
+            jpg: "image/jpeg",
+            jpeg: "image/jpeg",
+            png: "image/png",
+            webp: "image/webp",
+            avif: "image/avif",
+            tiff: "image/tiff",
+        }[extname] || "image/jpeg";
+
+    return contentType;
+}
+
 async function getImageBase64(folder, fileName) {
     const page = await getChromiumPage();
+    const imgType = getImgType(fileName);
 
     await page.goto(`https://pics.janitorai.com/${folder}/${fileName}`, { waitUntil: 'domcontentloaded' });
     const image = page.locator("img").nth(0);
-    const imageData = await image.evaluate((element) => {
+    const imageData = await image.evaluate((element, imgType) => {
         var cnv = document.createElement("canvas");
         cnv.width = element.naturalWidth;
         cnv.height = element.naturalHeight;
         cnv
             .getContext("2d")
             .drawImage(element, 0, 0, element.naturalWidth, element.naturalHeight);
-        return cnv.toDataURL().substring(22);
-    });
+        return cnv.toDataURL(imgType, 0.8).substring(22);
+    }, imgType);
     page.close();
     return imageData;
 }
 
 module.exports = {
-    getImageBase64
+    getImageBase64,
+    getImgType
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {getImageBase64 } = require('./crawl');
+const { getImageBase64, getImgType } = require('./crawl');
 const app = express();
 
 // Enable CORS
@@ -10,23 +10,11 @@ app.get("/health", (_req, res) => res.sendStatus(200));
 app.get("/proxy/:folder/:fileName", async (req, res, next) => {
   res.header("Cache-Control", "public, max-age=604800"); // 7 days in seconds
 
-  const {folder, fileName} = req.params; // folder can be bot-avatars or avatars
-  console.log({folder, fileName})
+  const { folder, fileName } = req.params; // folder can be bot-avatars or avatars
+  console.log({ folder, fileName })
 
-  const imageData = await getImageBase64(folder,fileName);
-
-  // Extract the file extension from the URL
-  const extname = fileName.split(".").pop();
-  const contentType =
-    {
-      jpg: "image/jpeg",
-      jpeg: "image/jpeg",
-      png: "image/png",
-      webp: "image/webp",
-      avif: "image/avif",
-      tiff: "image/tiff",
-    }[extname] || "application/octet-stream";
-
+  const imageData = await getImageBase64(folder, fileName);
+  const contentType = getImgType(fileName);
   // Set the response content type based on the file extension
   res.setHeader("Content-Type", contentType);
 
