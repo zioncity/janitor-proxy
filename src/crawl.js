5edf006155dddd06e09d43ecae20f064e96a3e59
dllt98 <135049225+dllt98@users.noreply.github.com>
2024-06-02 06:58:00 +0000

    fix image

--- src/crawl.js
+++ src/crawl.js
@@ -67,7 +67,7 @@ async function getImageBase64(folder, fileName) {
     const page = await getChromiumPage();
     const imgType = getImgType(fileName);
 
-    await page.goto(`https://ella.janitorai.com/${folder}/${fileName}`, { waitUntil: 'domcontentloaded' });
+    await page.goto(`https://ella.janitorai.com/${folder}/${fileName}?width=1000`, { waitUntil: 'domcontentloaded' });
     const image = page.locator("img").nth(0);
 
     const imageData = await image.evaluate((element, imgType) => {
